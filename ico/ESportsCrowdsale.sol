570b49ebc670fae13e5c37f633d07aa04fdd524d
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-18 12:09:53 +0300

    fixed error

--- ico/ESportsCrowdsale.sol
+++ ico/ESportsCrowdsale.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.16;
 import "./zeppelin/crowdsale/RefundableCrowdsale.sol";
 
 contract ESportsCrowdsale {
-	fucntion ESportsCrowdsale(
+	function ESportsCrowdsale(
 		uint32 _startTime,
         uint32 _endTime,
         address addressOfTokenUsedAsReward, //address _wallet,
