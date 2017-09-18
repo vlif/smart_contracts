0188fab4599f7f1c745f4bb309c92707f6e5be6f
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-18 11:56:55 +0300

    started ico

---
+++ ico/ESportsCrowdsale.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.16;
+
+import "./zeppelin/crowdsale/RefundableCrowdsale.sol";
+
+contract ESportsCrowdsale {
+	fucntion ESportsCrowdsale(
+		uint32 _startTime,
+        uint32 _endTime,
+        address addressOfTokenUsedAsReward, //address _wallet,
+	) {
+		
+	}
+}
