7230d326ab49b78a8428776513c33a4668422c08
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-18 18:38:32 +0300

    test try 2

--- ico/test_contract.sol
+++ ico/test_contract.sol
@@ -55,6 +55,17 @@ contract ERC20Basic {
 }
 
 /**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances. 
  */
