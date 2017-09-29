7cbe09017cfa45c8857e160c42a9087f396e8844
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-09-29 12:48:04 +0300

    ESportsMainProvider.sol refactoring

--- ico/ESportsMainCrowdsale.sol
+++ ico/ESportsMainCrowdsale.sol
@@ -5,17 +5,13 @@ import './zeppelin/math/SafeMath.sol';
 
 import "./ESportsConstants.sol";
 import "./ESportsToken.sol";
-// import "./ESportsRateProvider.sol";
 import "./ESportsBonusProvider.sol";
 
 contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
-    // uint constant minimalPurchase = 0.05 ether; // 50 000 000 000 000 000 Wei
-
 	// Overall 100.00% 60 000 000
     uint constant TEAM_BEN_TOKENS = 6000000 * TOKEN_DECIMAL_MULTIPLIER; // 20.00% // Founders
     uint constant TEAM_PHIL_TOKENS = 6000000 * TOKEN_DECIMAL_MULTIPLIER;
     uint constant COMPANY_COLD_STORAGE_TOKENS = 12000000 * TOKEN_DECIMAL_MULTIPLIER; // 20.00%
-	// it is separate wallet which is driven by the team and can be rewarded from to any investor the TEAM choose
     uint constant INVESTOR_TOKENS = 3000000 * TOKEN_DECIMAL_MULTIPLIER; // 5.00%
     uint constant BONUS_TOKENS = 3000000 * TOKEN_DECIMAL_MULTIPLIER; // 5.00% // pre-sale
 	uint constant BUFFER_TOKENS = 6000000 * TOKEN_DECIMAL_MULTIPLIER; // 10.00%
@@ -31,7 +27,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
     address constant PRE_SALE_ADDRESS = 0x00F1Eb3e6009De9460DcBaE5b2496a40c2DBE576;
     address btcBuyer = 0x0068536898Af4548b53ad23b9bbDaAD569bffdAA;
 
-    // ESportsRateProviderI public rateProvider;
     ESportsBonusProviderI public bonusProvider;
 
     bool private isInit = false;
@@ -51,11 +46,10 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
         _endTime, 
         240, // 240 ETR = 1 ETH at the rate 1 ETH = 250 EUR
         _hardCapTokens * TOKEN_DECIMAL_MULTIPLIER, // 60 000 000
-        _wallet, //=WALLET_ADDRESS
+        _wallet,
         _token,
         _softCapWei // _goal // 2 000 000 -> 8 000 ETH (250) -> 8 000 000 000 000 000 000 000 Wei
 	) {
-        // token.delegatecall(bytes4(sha3("transferOwnership(address)")), this);
 
 
 
@@ -66,30 +60,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
      */
     // function createTokenContract() internal returns(MintableToken) {
     //     return new ESportsToken();
-    // }
-
-    /**
-     * @dev Override getRate to integrate with rate provider.
-     */
-    // function getRate(uint _value) internal constant returns(uint) {
-    //     return rateProvider.getRate(msg.sender, soldTokens, _value, startTime);
-    // }
-
-    /**
-     * @dev Override getRateScale to integrate with rate provider.
-     */
-    // function getRateScale() internal constant returns(uint) {
-    //     return rateProvider.getRateScale();
-    // }
-
-    ///**
-    // * @dev Admin can set new rate provider.
-    // * @param _rateProviderAddress New rate provider.
-    // */
-    // function setRateProvider(address _rateProviderAddress) onlyOwner {
-    //     require(_rateProviderAddress != 0);
-
-    //     rateProvider = ESportsRateProviderI(_rateProviderAddress);
     // }
 
     /**
@@ -141,28 +111,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
         }
     }
 
-    // function setBonusProvider(address _bonusProviderAddress) onlyOwner {
-    //     require(_bonusProviderAddress != 0);
-
-    //     bonusProvider = ESportsBonusProviderI(_bonusProviderAddress);
-    // }
-
-	///**
-    // * @dev Admin can move end time.
-    // * @param _endTime New end time.
-    // */
-    // function setEndTime(uint32 _endTime) onlyOwner {
-    //     endTime = _endTime;
-    // }
-
-    // function validPurchase(uint _amountWei, uint _actualRate, uint _totalSupply) internal constant returns (bool) {
-    //     if (_amountWei < minimalPurchase) {
-    //         return false;
-    //     }
-
-    //     return super.validPurchase(_amountWei, _actualRate, _totalSupply);
-    // }
-
     /**
      * @dev Finish the crowdsale
      */
@@ -186,10 +134,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
     function init() onlyOwner public returns(bool) {
         require(!isInit);
 
-        // ESportsRateProvider rProvider = new ESportsRateProvider();
-        // rProvider.transferOwnership(owner);
-        // rateProvider = rProvider;
-
         ESportsBonusProvider bProvider = new ESportsBonusProvider(ESportsToken(token), COMPANY_COLD_STORAGE_ADDRESS);
         // bProvider.transferOwnership(owner);
         bonusProvider = bProvider;
@@ -204,7 +148,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
         token.mint(BONUS_ADDRESS, BONUS_TOKENS);
         token.mint(bonusProvider, BUFFER_TOKENS); // mint bonus tokent to bonus provider
 
-        // ESportsToken(token).addExcluded(TEAM_ADDRESS);
         ESportsToken(token).addExcluded(INVESTOR_ADDRESS);
         ESportsToken(token).addExcluded(BONUS_ADDRESS);
         ESportsToken(token).addExcluded(COMPANY_COLD_STORAGE_ADDRESS);
@@ -220,9 +163,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
      * @dev Mint of tokens in the name of the founders and freeze part of them
      */
     function mintToFounders() onlyOwner internal returns(bool) {
-        // ESportsToken(token).mintAndFreezePart(TEAM_ADDRESS, TEAM_TOKENS, 50, _startTime + 5 * 1 minutes); //+1 years
-        // token.mint(TEAM_ADDRESS, TEAM_TOKENS);
-
         ESportsToken(token).mintTimelocked(TEAM_BEN_ADDRESS, TEAM_BEN_TOKENS.mul(20).div(100), startTime + 1 years); //minutes
         ESportsToken(token).mintTimelocked(TEAM_BEN_ADDRESS, TEAM_BEN_TOKENS.mul(30).div(100), startTime + 3 years); //minutes
         ESportsToken(token).mintTimelocked(TEAM_BEN_ADDRESS, TEAM_BEN_TOKENS.mul(30).div(100), startTime + 5 years); //minutes
@@ -241,8 +181,6 @@ contract ESportsMainCrowdsale is usingESportsConstants, RefundableCrowdsale {
      */
     function buyForBitcoin(address _beneficiary, uint _amountWei) public returns(bool) {
         require(msg.sender == btcBuyer);
-        // require(_beneficiary != 0x0);
-        // require(_amountWei > 0);
 
         buyTokens(_beneficiary, _amountWei);
         return true;
