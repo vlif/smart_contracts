16c1cd0f4e8efc40b39fa9599abcbea57e86449d
Eduard Lunin <eduard.lunin@dataart.com>
2017-10-20 13:26:18 +0300

    small changes

--- sale/freezingStorage.sol
+++ sale/freezingStorage.sol
@@ -55,7 +55,7 @@ contract FreezingStorage is Ownable {
     			// uint realHonoredTokenAmount = (tokenAmount * (cryptosale.weiRaised() * 100 / cryptosale.goal())) / 100;
     			uint k =  100 - (100 - (weiRaised * 100 / goal)) / 2;
     			uint realHonoredTokenAmount = tokenAmount * k / 100;
-
+				
     			token.transfer(forwardWallet, realHonoredTokenAmount);
     			token.transfer(backwordWallet, tokenAmount.sub(realHonoredTokenAmount));
 			} else {
