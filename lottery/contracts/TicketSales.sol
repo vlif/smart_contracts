32324f0b0bc1356dd13b068c62f87c56455f4b97
Vasiliy Lifanovskiy <vlif.developer@gmail.com>
2017-10-23 20:55:06 +0300

    test lottery

--- lottery/contracts/TicketSales.sol
+++ lottery/contracts/TicketSales.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.15;
 
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import './zeppelin-solidity/contracts/math/SafeMath.sol';
 import './storages/Ticket.sol';
 import './storages/Fund.sol';
 
